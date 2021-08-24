7e4ad81ce1a4535575262e310a0819c32438bc64
Michaela Sivertsen <mjsivertsen@msn.com>
2021-08-23 19:39:21 -0600

    made notes work

--- app/controllers/notes_controller.rb
+++ app/controllers/notes_controller.rb
@@ -1,2 +1,6 @@
 class NotesController < ApplicationController
+  def index
+    notes = Note.all
+    render component: "Notes", props: {notes: notes}
+  end
 end
---
+++ app/javascript/components/Notes.js
@@ -0,0 +1,21 @@
+import React from 'react'
+
+const Notes = (props) => {
+  return (
+    <div>
+    <h1>Post-It-Notes</h1>
+    <p>Post-It-Notes might be found here</p>
+    {props.notes.map((note) => {
+      return (
+      <div key={note.id}>
+        <p>{note.title}</p>
+        <p>{note.description}</p>
+      </div>
+      );
+    }
+    )}
+  </div>
+  );
+};
+
+export default Notes;
